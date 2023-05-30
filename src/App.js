bd578a413fc9d1b52b65158e90ebb08bb8b31e16
jbowdle <johnwbowdle@gmail.com>
2023-05-30 15:24:56 -0500

    Removed unneeded code from app.js

--- src/App.js
+++ src/App.js
@@ -2,29 +2,6 @@ import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import ProjectCard from "./components/ProjectCard";
 
-// Clicking the burger icon will open and close nav
-// function handleBurgerClick() {
-//   const navLinks = document.querySelectorAll(".nav-link");
-//   for (const link of navLinks){
-//     if (link.style.display === "block") {
-//       link.style.display = "none";
-//     } else {
-//       link.style.display = "block";
-//     }
-//   }
-// }
-
-
-
-// function collapseNav() {
-//   const navLinks = document.querySelectorAll(".nav-link");
-//   for (const link of navLinks){
-//     if (link.style.display === "block") {
-//       link.style.display = "none";
-//     }
-//   }
-// }
-
 function App(props) {
   const projectList = props.projects.map((project) => (
     <ProjectCard id={project.id} image={project.image} alt={project.alt} name={project.name} key={project.id}/>
