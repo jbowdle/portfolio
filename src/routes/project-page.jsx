d92e3766452d275d76767945894b753a84a5f135
jbowdle <johnwbowdle@gmail.com>
2023-06-06 14:36:16 -0500

    created example HTML for project pages

--- src/routes/project-page.jsx
+++ src/routes/project-page.jsx
@@ -1,7 +1,18 @@
 export default function ProjectInfo() {
   return (
+    // example HTML
     <div>
-      <h1>This is an example project page</h1>
+      <div className="project-gallery">
+        <img src="./assets/placeholder.png"/>
+      </div>
+      <div className="project-info">
+        <h1>Project title</h1>
+        <div>
+          <p>Project Description</p>
+          <a>Link to Project Repo</a>
+          <a>Link to deployed Project</a>
+        </div>
+      </div>
     </div>
   )
 }
