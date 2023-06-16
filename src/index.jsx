1ed710b2cc64b7a602c2820c8cc2c19062234cc7
jbowdle <johnwbowdle@gmail.com>
2023-06-15 21:17:40 -0500

    moved project data to separate js file

--- src/index.jsx
+++ src/index.jsx
@@ -11,57 +11,7 @@ import About from "./routes/about";
 import Portfolio from "./routes/portfolio";
 import Resume from "./routes/resume";
 import ProjectPage, { loader as projectLoader } from "./routes/project-page"
-
-const PROJECT_DATA = [
-  { 
-    name: "Example-Project-1", 
-    id: "project-0", image: "assets/placeholder.png", alt: "Placeholder image for an example project",
-    tags: ["tag1", "tag2", "tag3"],  
-    desc: "replace this desc!",
-    repoLink: "https://github.com/jbowdle/portfolio",
-    deployedLink: "https://github.com/jbowdle/portfolio", 
-  },
-  {
-    name: "Example-Project-2", 
-    id: "project-1", image: "assets/placeholder.png", alt: "Placeholder image for an example project", 
-    tags: ["tag1", "tag2", "tag3"], 
-    desc: "replace this desc!",
-    repoLink: "https://github.com/jbowdle/portfolio",
-    deployedLink: "https://github.com/jbowdle/portfolio",  
-  },
-  { 
-    name: "Example-Project-3",
-    id: "project-2", image: "assets/placeholder.png", alt: "Placeholder image for an example project",  
-    tags: ["tag1", "tag2", "tag3"], 
-    desc: "replace this desc!",
-    repoLink: "https://github.com/jbowdle/portfolio",
-    deployedLink: "https://github.com/jbowdle/portfolio",  
-  },
-  { 
-    name: "Example-Project-4", 
-    id: "project-3", image: "assets/placeholder.png", alt: "Placeholder image for an example project", 
-    tags: ["tag1", "tag2", "tag3"], 
-    desc: "replace this desc!",
-    repoLink: "https://github.com/jbowdle/portfolio",
-    deployedLink: "https://github.com/jbowdle/portfolio",  
-  },
-  { 
-    name: "Example-Project-5",
-    id: "project-4", image: "assets/placeholder.png", alt: "Placeholder image for an example project",  
-    tags: ["tag1", "tag2", "tag3"], 
-    desc: "replace this desc!",
-    repoLink: "https://github.com/jbowdle/portfolio",
-    deployedLink: "https://github.com/jbowdle/portfolio",  
-  },
-  { 
-    name: "Example-Project-6", 
-    id: "project-5", image: "assets/placeholder.png", alt: "Placeholder image for an example project", 
-    tags: ["tag1", "tag2", "tag3"], 
-    desc: "replace this desc!",
-    repoLink: "https://github.com/jbowdle/portfolio",
-    deployedLink: "https://github.com/jbowdle/portfolio",  
-  },
-]
+import PROJECT_DATA from "./projectData";
 
 const router = createBrowserRouter([
   {
---
+++ src/projectData.js
@@ -0,0 +1,52 @@
+const PROJECT_DATA = [
+  { 
+    name: "Example-Project-1", 
+    id: "project-0", image: "assets/placeholder.png", alt: "Placeholder image for an example project",
+    tags: ["tag1", "tag2", "tag3"],  
+    desc: "replace this desc!",
+    repoLink: "https://github.com/jbowdle/portfolio",
+    deployedLink: "https://github.com/jbowdle/portfolio", 
+  },
+  {
+    name: "Example-Project-2", 
+    id: "project-1", image: "assets/placeholder.png", alt: "Placeholder image for an example project", 
+    tags: ["tag1", "tag2", "tag3"], 
+    desc: "replace this desc!",
+    repoLink: "https://github.com/jbowdle/portfolio",
+    deployedLink: "https://github.com/jbowdle/portfolio",  
+  },
+  { 
+    name: "Example-Project-3",
+    id: "project-2", image: "assets/placeholder.png", alt: "Placeholder image for an example project",  
+    tags: ["tag1", "tag2", "tag3"], 
+    desc: "replace this desc!",
+    repoLink: "https://github.com/jbowdle/portfolio",
+    deployedLink: "https://github.com/jbowdle/portfolio",  
+  },
+  { 
+    name: "Example-Project-4", 
+    id: "project-3", image: "assets/placeholder.png", alt: "Placeholder image for an example project", 
+    tags: ["tag1", "tag2", "tag3"], 
+    desc: "replace this desc!",
+    repoLink: "https://github.com/jbowdle/portfolio",
+    deployedLink: "https://github.com/jbowdle/portfolio",  
+  },
+  { 
+    name: "Example-Project-5",
+    id: "project-4", image: "assets/placeholder.png", alt: "Placeholder image for an example project",  
+    tags: ["tag1", "tag2", "tag3"], 
+    desc: "replace this desc!",
+    repoLink: "https://github.com/jbowdle/portfolio",
+    deployedLink: "https://github.com/jbowdle/portfolio",  
+  },
+  { 
+    name: "Example-Project-6", 
+    id: "project-5", image: "assets/placeholder.png", alt: "Placeholder image for an example project", 
+    tags: ["tag1", "tag2", "tag3"], 
+    desc: "replace this desc!",
+    repoLink: "https://github.com/jbowdle/portfolio",
+    deployedLink: "https://github.com/jbowdle/portfolio",  
+  },
+];
+
+export default PROJECT_DATA;
