d0de5ae2d313a11dc290e76f466a236a8b39455f
jbowdle <johnwbowdle@gmail.com>
2023-07-24 19:14:21 -0500

    added dungeon finderrz project

--- src/projectData.js
+++ src/projectData.js
@@ -9,8 +9,16 @@ const PROJECT_DATA = [
     deployedLink: "", 
   },
   {
+    name: "Dungeon Finderrz", 
+    id: "project-1", image: "assets/dungeon-finderrz.png", alt: "Placeholder image for an example project",
+    tags: [],  
+    desc: "Finds friends to play Dungeons and Dragons with!",
+    repoLink: "https://github.com/WolfSpiderman/DungeonFinderrz",
+    deployedLink: "https://dungeon-finderrz-e6d696a81520.herokuapp.com/games", 
+  },
+  {
     name: "Fullscreen Futol Fan", 
-    id: "project-1", image: "assets/fullscreen-futbol-fan-screenshot.png", alt: "Placeholder image for an example project", 
+    id: "project-2", image: "assets/fullscreen-futbol-fan-screenshot.png", alt: "Placeholder image for an example project", 
     tags: [], 
     desc: "Track games and stats for your favorite Major League Soccer teams.",
     repoLink: "https://github.com/jbowdle/Fullscreen-Futbol-Fan",
@@ -18,7 +26,7 @@ const PROJECT_DATA = [
   },
   { 
     name: "Content Management System",
-    id: "project-2", image: "assets/placeholder.png", alt: "Placeholder image for an example project",  
+    id: "project-3", image: "assets/placeholder.png", alt: "Placeholder image for an example project",  
     tags: [], 
     desc: "This is a basic employee database management system.",
     repoLink: "https://github.com/jbowdle/content-management-system",
@@ -26,7 +34,7 @@ const PROJECT_DATA = [
   },
   { 
     name: "Logo Generator", 
-    id: "project-3", image: "assets/placeholder.png", alt: "Placeholder image for an example project", 
+    id: "project-4", image: "assets/placeholder.png", alt: "Placeholder image for an example project", 
     tags: [], 
     desc: "Easily generate logos for your website with this app.",
     repoLink: "https://github.com/jbowdle/Logo-Generator",
@@ -34,7 +42,7 @@ const PROJECT_DATA = [
   },
   { 
     name: "Pomodoro Timer",
-    id: "project-4", image: "assets/placeholder.png", alt: "Placeholder image for an example project",  
+    id: "project-5", image: "assets/placeholder.png", alt: "Placeholder image for an example project",  
     tags: [], 
     desc: "Basic pomodoro timer, work in progress.",
     repoLink: "https://github.com/jbowdle/Pomodoro-Timer",
@@ -42,7 +50,7 @@ const PROJECT_DATA = [
   },
   { 
     name: "Social Network API", 
-    id: "project-5", image: "assets/placeholder.png", alt: "Placeholder image for an example project", 
+    id: "project-6", image: "assets/placeholder.png", alt: "Placeholder image for an example project", 
     tags: [], 
     desc: "Backend for a social network",
     repoLink: "https://github.com/jbowdle/social-network-api",
